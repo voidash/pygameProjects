7a0c0d52678aa03b9be83a4e083bec8d2c566a04
Ashish Thapa <ashish.thapa477@gmail.com>
2020-03-31 17:16:29 +0545

    fixed the bug of not showing tiles properly, and added collision feature with 8 way movement for player

--- Tiler/sprites.py
+++ Tiler/sprites.py
@@ -2,37 +2,58 @@ import pygame as pg
 from settings import *
 
 class Player(pg.sprite.Sprite):
-    def __init__(self,game,x,y):
+    def __init__(self, game, x, y):
         self.groups = game.all_sprites
-        pg.sprite.Sprite.__init__(self,self.groups)
-        self.game=game
-        self.image =pg.Surgace((TILESIZE,TILESIZE))
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(YELLOW)
-        self.rect= self.image.get_rect()
-        self.x = x
-        self.y = y
+        self.rect = self.image.get_rect()
+        self.vx,self.vy =0,0
+        self.x = x *TILESIZE
+        self.y = y *TILESIZE
 
-    def move(self,dx=0,dy=0):
-        self.x +=dx
-        self.y +=dy
-    
+
+    def get_keys(self):
+        self.vx,self.vy=0,0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vx = -PLAYERSPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vx = PLAYERSPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vy = -PLAYERSPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vy = PLAYERSPEED
+        if self.vy != 0 and self.vx != 0:
+            self.vy *= 0.7071
+            self.vx *= 0.7071
+    def move(self, dx=0, dy=0):
+        if not self.collide_with_walls(dx,dy):
+            self.x += dx
+            self.y += dy
+
+    def collide_with_walls(self,dx=0,dy=0):
+        for wall in self.game.walls:
+            if wall.x == self.x+dx and wall.y == self.y + dy:
+                return True
+        return False
+
     def update(self):
-        self.rect.x = self.x * TILESIZE
-        self.rect.y = self.y * TILESIZE
+        self.get_keys()
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+        self.rect.topleft = (self.x,self.y)
 
 class Wall(pg.sprite.Sprite):
-    def __init__(self,game,x,y):
+    def __init__(self, game, x, y):
         self.groups = game.all_sprites, game.walls
-        pg.sprite.Sprite.__init__(self,self.groups)
+        pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pg.Surface((TILESIZE,TILESIZE))
+        self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.x=x
-        self.y=y
-        self.rect.x = x*TILESIZE
-        self.rect.y = y*TILESIZE
-
-        
-
-
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
